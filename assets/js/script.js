c3cdbd22ce6da88d3144d8132f1cfa9c4c6eca52
JustAri <ariichoua@gmail.com>
2025-05-12 16:58:21 +0200

    ajout de listener pour ouvrir le modal et le fermer ainsi que du css pour le modal

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const links = document.querySelectorAll('[data-modal]'); // On récupere les liens d'ouverture des modals
+const modalFilter = document.getElementById('modalFilter'); // Et le filtre modal
+
+//Pour chaque lien on ajoute un listener au click 
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // suppréssion du comportement par défaut (ici du lien)
+
+        const modal = document.getElementById(link.dataset.modal); // on récupére le modal grace a l'id
+        modal.style.display = "flex";                              // stocké dans le data-modal puis on l'affiche
+        modalFilter.style.display = "block"
+    })
+});
+
+const closebuttons = document.querySelectorAll('[data-close]'); // récupération des liens de fermeture du modal
+
+// pour chaque liens de fermeture on supprime le comportement par défaut et on fait disparaitre le modal et le filtre
+closebuttons.forEach(button => { 
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const modal = button.closest('.modal')
+        modal.style.display = "none";
+        modalFilter.style.display = "none"
+    })
+});
+
+// const form = document.getElementById('registrationForm');
+
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     const target = e.currentTarget;
+// })
