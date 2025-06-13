c01b382dc81d61130a9dce56acb8bff248787fd9
JustAri <ariichoua@gmail.com>
2025-06-13 13:19:47 +0200

    refactorisation de la validation des champs avec une fonction

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -1,6 +1,5 @@
 const form = document.getElementById('registrationForm'); // On récupere le formulaire
 
-
 form.addEventListener('submit', function (e) { // Quand le formulaire est soumis
     e.preventDefault();  // On enleve le comportement par défaut
 
@@ -11,7 +10,38 @@ form.addEventListener('submit', function (e) { // Quand le formulaire est soumis
     Array.from(inputs).forEach(input => { // On copie les elements de l'objet dans un tableau puis 
                                           // on effectue les vérification ci dessous pour chaque élément
         
-        if (input.type === "submit") { // si l'élément est de type submit (generalement le bouton) on passe au suivant
+        validateInput(input);
+    });
+
+    if (validate === true) { // si la variable est true apres les vérifications ci dessus 
+        console.log('validé!')
+        target.submit(); // on envoie le formulaire
+    }
+    
+})
+
+function resetInputError(input) { // fonction appelé pour enlever les messages d'erreurs si le champ est valide
+    const errorDiv = input.parentElement.querySelector('.error'); 
+    errorDiv.textContent = "";
+            input.classList.remove('invalid');
+} 
+
+const inputs = document.querySelectorAll('form input'); // on récupere tout les inputs
+
+inputs.forEach(input => {
+    
+    input.addEventListener('input', function () {
+
+        validateInput(input);
+    })
+
+    
+});
+
+
+function validateInput(input) {
+
+if (input.type === "submit") { // si l'élément est de type submit (generalement le bouton) on passe au suivant
 
             return;
         }
@@ -37,20 +67,7 @@ form.addEventListener('submit', function (e) { // Quand le formulaire est soumis
             
             errorDiv.textContent = input.dataset.error; // Et on fait apparaitre un message d'erreur
 
-            input.classList.add('invalid')
+            input.classList.add('invalid');
         }
-    
-    });
 
-    if (validate === true) { // si la variable est true apres les vérifications ci dessus 
-        console.log('validé!')
-        target.submit(); // on envoie le formulaire
-    }
-    
-})
-
-function resetInputError(input) {
-    const errorDiv = input.parentElement.querySelector('.error'); 
-    errorDiv.textContent = "";
-            input.classList.remove('invalid');
 }
